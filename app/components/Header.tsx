a96c1fb2649729433005407ddee4cf841dc51413
Siran Cao <siranchao@gmail.com>
2023-06-19 23:31:29 -0400

    finish intro section

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,9 @@
-
+import Link from "next/link"
 
 export default function Header() {
 
     return (
-        <div className="navbar bg-transparent mb-16">
+        <div className="sticky top-0 navbar bg-transparent mb-16 backdrop-blur-sm">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -16,7 +16,7 @@ export default function Header() {
                     <li><a>Sign in</a></li>
                 </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">Siran.dev</a>
+                <Link href="/" className="btn btn-ghost normal-case text-xl">Siran.dev</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-import Image from 'next/image'
+import Intro from "./components/Intro"
 
 export default function Home() {
   return (
+    <>
+      <Intro />
+      <div className="min-h-screen"></div>
+    
+    </>
+
+
+
+
     // <main className="flex flex-col items-center justify-between">
     //   <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
     //     <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -109,6 +118,6 @@ export default function Home() {
     //     </a>
     //   </div>
     // </main>
-    <><div className="bg-gradient-to-r from-indigo-500 to-purple-500 min-h-screen ">Hello</div></>
+    
   )
 }
