b41c0e683460f69b4f12e129b578c1b0f0590b68
Siran Cao <siranchao@gmail.com>
2023-07-06 20:36:53 -0400

    add create new post page

--- app/newPost/page.tsx
+++ app/newPost/page.tsx
@@ -1,9 +1,35 @@
 'use client';
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { useForm, SubmitHandler } from "react-hook-form"
+
+
+interface FormData {
+    title: string;
+    subtitle: string;
+    info: string;
+    iconUrl?: string;
+    primaryImgUrl?: string;
+    secondaryImgUrl?: string;
+    videoUrl?: string;
+    mainText?: string;
+    primaryLink: string;
+    primaryUrl: string;
+    secondaryLink?: string;
+    secondaryUrl?: string;
+}
 
 export default async function NewPost() {
     const { data: session } = useSession()
+
+    const { register, handleSubmit, reset } = useForm<FormData>()
+    const onSubmit = handleSubmit((data) => {
+        console.log(data)
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    })
 
     if (!session?.user.isAdmin) {
         return (
@@ -22,8 +48,69 @@ export default async function NewPost() {
 
 
     return (
-        <>
-            Post new article
-        </>
+        <main className="mb-20">
+            <p className="text-2xl mb-4 font-bold tracking-wide">Create a new post</p>
+
+            <form className="flex flex-col gap-4 border border-gray-300 p-8 rounded-lg shadow-lg" onSubmit={onSubmit}>
+                <div>
+                    <label className="label">Post Title</label>
+                    <input type="text" placeholder="show as main title" className="input input-bordered w-full max-w-lg dark:bg-transparent dark:text-gray-300 dark:border-gray-300" {...register("title")} required/>
+                </div>
+
+                <div>
+                    <label className="label">Post Subtitle</label>
+                    <input type="text" placeholder="show as sub-title" className="input input-bordered w-full max-w-lg dark:bg-transparent dark:text-gray-300 dark:border-gray-300" {...register("subtitle")} required/>
+                </div>
+
+                <div>
+                    <label className="label">Post Info</label>
+                    <textarea className="textarea textarea-bordered w-full max-w-lg dark:bg-transparent dark:text-gray-300 dark:border-gray-300" placeholder="show as post description" {...register("info")} required></textarea>
+                </div>
+
+                <div>
+                    <label className="label">Post Main Text</label>
+                    <textarea className="textarea textarea-bordered w-full max-w-lg dark:bg-transparent dark:text-gray-300 dark:border-gray-300" placeholder="show as post main text" {...register("mainText")}></textarea>
+                </div>
+
+                <div>
+                    <label className="label">Primary Link</label>
+                    <input type="text" placeholder="link name" className="input input-bordered w-full max-w-xs dark:bg-transparent dark:text-gray-300 dark:border-gray-300 m-1" {...register("primaryLink")} required/>
+                    <input type="text" placeholder="url" className="input input-bordered w-full max-w-xs dark:bg-transparent dark:text-gray-300 dark:border-gray-300 m-1" {...register("primaryUrl")} required/>
+                </div>
+
+                <div>
+                    <label className="label">Secondary Link</label>
+                    <input type="text" placeholder="link name" className="input input-bordered w-full max-w-xs dark:bg-transparent dark:text-gray-300 dark:border-gray-300 m-1" {...register("secondaryLink")} />
+                    <input type="text" placeholder="url" className="input input-bordered w-full max-w-xs dark:bg-transparent dark:text-gray-300 dark:border-gray-300 m-1" {...register("secondaryUrl")} />
+                </div>
+
+                <div>
+                    <label className="label">Video URL</label>
+                    <input type="text" placeholder="url" className="input input-bordered w-full max-w-lg dark:bg-transparent dark:text-gray-300 dark:border-gray-300" {...register("videoUrl")} />
+                </div>
+
+                <hr/>
+
+                <div>
+                    <label className="label">Logo Image</label>
+                    <input type="file" className="file-input file-input-sm file-input-bordered w-full max-w-xs dark:bg-transparent dark:text-gray-300 dark:border-gray-300" />
+                </div>
+
+                <div>
+                    <label className="label">Primary Image</label>
+                    <input type="file" className="file-input file-input-sm file-input-accent file-input-bordered w-full max-w-xs dark:bg-transparent dark:text-gray-300 dark:border-gray-300" />
+                </div>
+
+                <div>
+                    <label className="label">Secondary Image</label>
+                    <input type="file" className="file-input file-input-sm file-input-info file-input-bordered w-full max-w-xs dark:bg-transparent dark:text-gray-300 dark:border-gray-300" />
+                </div>
+
+                <div className="flex gap-2 pt-4 justify-end">
+                    <button className="btn btn-sm btn-outline" type="submit">Submit</button>
+                    <button className="btn btn-sm btn-outline btn-error" type="reset" onClick={() => reset()}>Reset</button>
+                </div>
+            </form>
+        </main>
     )
 }
